ccd5559d12a82ff60a81d1935926abf3afac7aaa
Alexey <enslaver@list.ru>
2021-02-11 00:33:47 +0300

    replaced to "ru.geekbrains.arhitecture.patterns.objectmapper.orm.mappers" converted from class to interface added getEmptyElement() method

--- architectural-system-patterns/src/main/java/ru/geekbrains/arhitecture/patterns/objectmapper/dao/mappers/template/Mapper.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.arhitecture.patterns.objectmapper.dao.mappers.template;
-
-import java.util.List;
-
-public abstract class Mapper<O> {
-    public abstract O setFields(List<Field> fields);
-    public abstract List<Field> getFields(O entity);
-    public abstract List<Field> getID(O entity);
-}
