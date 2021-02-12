ab7c95dcc945e2e0f36aa3758ba3a7aacc7da617
Alexey <enslaver@list.ru>
2021-02-12 17:15:08 +0300

    replaced from "ru.geekbrains.arhitecture.patterns.objectmapper.orm" to "ru.geekbrains.arhitecture.patterns.objectmapper.dao.orm"
    updated imports
    
    rewrite method "getFieldsAsString"
    
    removed method "extractField"
    
    renamed protected interface from "Function<T>" to "ExecFunction<T>"

--- architectural-system-patterns/src/main/java/ru/geekbrains/arhitecture/patterns/objectmapper/dao/orm/RepositoryImpl.java
+++ architectural-system-patterns/src/main/java/ru/geekbrains/arhitecture/patterns/objectmapper/dao/orm/RepositoryImpl.java
@@ -1,13 +1,14 @@
-package ru.geekbrains.arhitecture.patterns.objectmapper.orm;
+package ru.geekbrains.arhitecture.patterns.objectmapper.dao.orm;
 
-import ru.geekbrains.arhitecture.patterns.objectmapper.orm.exception.UncheckedSQLException;
-import ru.geekbrains.arhitecture.patterns.objectmapper.orm.mappers.Field;
-import ru.geekbrains.arhitecture.patterns.objectmapper.orm.mappers.Mapper;
+import ru.geekbrains.arhitecture.patterns.objectmapper.dao.orm.exception.UncheckedSQLException;
+import ru.geekbrains.arhitecture.patterns.objectmapper.dao.orm.mappers.Field;
+import ru.geekbrains.arhitecture.patterns.objectmapper.dao.orm.mappers.Mapper;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RepositoryImpl<E> implements Repository<E> {
@@ -28,45 +29,42 @@ public class RepositoryImpl<E> implements Repository<E> {
         this.tableName = (tableName.isBlank()) ? this.getClass().getSimpleName() : tableName;
 
         this.initSqlQueryTemplates();
-
-//        System.out.println("selectAllQuery = " + selectAllQuery);
-//        System.out.println("deletePrepQuery = " + deletePrepQuery);
-//        System.out.println("updatePrepQuery = " + updatePrepQuery);
-//        System.out.println("insertPrepQuery = " + insertPrepQuery);
     }
 
     private void initSqlQueryTemplates() {
         E emptyEntity = this.mapper.getEmptyElement();
         List<Field> fields = this.mapper.getAllFields(emptyEntity);
         List<Field> id = this.mapper.getID(emptyEntity);
-        List<Field> fieldsWithoutId = fields.stream().filter(field -> id.stream().noneMatch(field::equals)).collect(Collectors.toList());
+        List<Field> fieldsWithoutId = fields.stream()
+                .filter(field -> id.stream().noneMatch(field::equals))
+                .collect(Collectors.toList());
 
         this.selectAllQuery = String.format(
                 "SELECT %s FROM %s",
-                this.getFieldsAsString(fields, "%s, %s"),
+                this.getFieldsAsString(fields,Field::getName, "%s, %s"),
                 this.tableName);
 
         this.deletePrepQuery = String.format(
                 "DELETE FROM %s WHERE %s",
                 this.tableName,
-                this.getFieldsAsString(id, "%s = ? AND %s = ?"));
+                this.getFieldsAsString(id, (field)->field.getName() + " = ?", "%s AND %s"));
 
         this.updatePrepQuery = String.format(
                 "UPDATE %s SET %s WHERE %s",
                 this.tableName,
-                this.getFieldsAsString(fieldsWithoutId, "%s = %s"),
-                this.getFieldsAsString(id, "%s = ? AND %s = ?"));
+                this.getFieldsAsString(fieldsWithoutId, Field::getName,"%s, %s"),
+                this.getFieldsAsString(id, (field)->field.getName() + " = ?", "%s AND %s "));
 
 
         this.insertPrepQuery = String.format(
                 "INSERT INTO %s (%s) VALUES (%s)",
                 this.tableName,
-                this.getFieldsAsString(fields, "%s, %s"),
-                this.getFieldsAsString(fields, "?, ?"));
+                this.getFieldsAsString(fields, Field::getName, "%s, %s"),
+                this.getFieldsAsString(fields, (field)->"?", "%s, %s"));
     }
 
-    private String getFieldsAsString(List<Field> fields, String reducedSchema) {
-        return fields.stream().map(Field::getName).reduce((f1, f2)->String.format(reducedSchema, f1, f2)).get();
+    private String getFieldsAsString(List<Field> fields, Function<Field, String> fieldToString, String reducedSchema) {
+        return fields.stream().map(fieldToString).reduce((f1, f2)->String.format(reducedSchema, f1, f2)).get();
     }
 
     private Connection getConnection() {
@@ -109,7 +107,7 @@ public class RepositoryImpl<E> implements Repository<E> {
             List<Field> fields = this.mapper.getAllFields(entity);
             this.addValueToPrepareStatement(statement, 1, fields);
 
-            statement.executeQuery();
+            statement.executeUpdate();
         });
     }
 
@@ -181,8 +179,8 @@ public class RepositoryImpl<E> implements Repository<E> {
     @Override
     public List<E> findByFields(List<Field> fields) {
         String findQuery = String.format(
-                this.insertPrepQuery + " WHERE %s",
-                this.getFieldsAsString(fields, "%s = ? AND %s = ?"));
+                this.selectAllQuery + " WHERE %s",
+                this.getFieldsAsString(fields, field -> field.getName() + " = ?","%s AND %s"));
         List<E> result = new ArrayList<>();
 
         this.executePreparedStatement(findQuery, statement -> {
@@ -195,9 +193,9 @@ public class RepositoryImpl<E> implements Repository<E> {
     @Override
     public List<E> findByFieldsORFields(List<List<Field>> fieldOrCondition) {
         String findQuery = String.format(
-                this.insertPrepQuery + " WHERE %s",
-                fieldOrCondition.stream().map(fields -> this.getFieldsAsString(fields, "%s = ? AND %s = ?"))
-                .reduce((leftCondition, rightCondition)->String.format("(%s = ? OR %s = ?)", leftCondition, rightCondition)));
+                this.selectAllQuery + " WHERE %s",
+                fieldOrCondition.stream().map(fields -> this.getFieldsAsString(fields, field -> field.getName() + " = ?", "%s AND %s"))
+                .reduce((leftCondition, rightCondition)->String.format("(%s) OR (%s)", leftCondition, rightCondition)));
 
         List<E> result = new ArrayList<>();
 
@@ -215,54 +213,47 @@ public class RepositoryImpl<E> implements Repository<E> {
     }
 
     private int addValueToPrepareStatement(PreparedStatement preparedStatement,int valueStartIndex, List<Field> fields) throws SQLException {
+        int valuePosition = valueStartIndex;
         for (Field field : fields) {
-            preparedStatement.setObject(valueStartIndex++, field.getValue());
+            preparedStatement.setObject(valuePosition++, field.getValue());
         }
-        return valueStartIndex;
+        return valuePosition;
     }
 
     private List<E> resultSetToEntities(ResultSet resultSet) throws SQLException {
         List<E> result = new LinkedList<>();
         while (resultSet.next()) {
-            result.add(this.mapper.createFromFields(this.extractField(resultSet)));
+            result.add(this.mapper.create(resultSet));
         }
         return result;
     }
 
-    private List<Field> extractField(ResultSet resultSet) throws SQLException {
-        List<Field> fields = this.mapper.getAllFields(this.mapper.getEmptyElement());
-        for (Field field : fields) {
-            field.setValue(resultSet.getObject(field.getName(), field.getValueType()));
-        }
-        return fields;
-    }
-
-    protected final void executeStatement(Function<Statement> function) {
+    protected final void executeStatement(ExecFunction<Statement> execFunction) {
         try (Statement statement = this.getConnection().createStatement()) {
-            function.execute(statement);
+            execFunction.execute(statement);
         } catch (SQLException e) {
             throw new UncheckedSQLException(e);
         }
     }
 
-    protected final void executePreparedStatement(String preparedString, Function<PreparedStatement> fillingFunction) {
+    protected final void executePreparedStatement(String preparedString, ExecFunction<PreparedStatement> fillingExecFunction) {
         try (PreparedStatement preparedStatement = getConnection().prepareStatement(preparedString)) {
 
-            fillingFunction.execute(preparedStatement);
+            fillingExecFunction.execute(preparedStatement);
 
         } catch (SQLException e) {
             throw new UncheckedSQLException(e);
         }
     }
 
-    protected final void executeBatchPreparedStatement(String preparedString, Function<PreparedStatement> fillingFunction) {
+    protected final void executeBatchPreparedStatement(String preparedString, ExecFunction<PreparedStatement> fillingExecFunction) {
         try (PreparedStatement preparedStatement = getConnection().prepareStatement(preparedString)) {
             Connection connection = preparedStatement.getConnection();
 
             try {
                 connection.setAutoCommit(false);
 
-                fillingFunction.execute(preparedStatement);
+                fillingExecFunction.execute(preparedStatement);
 
                 connection.commit();
             } catch (BatchUpdateException e) {
@@ -277,7 +268,8 @@ public class RepositoryImpl<E> implements Repository<E> {
     }
 
     @FunctionalInterface
-    protected interface Function<T> {
+    protected interface ExecFunction<T> {
         void execute(T statement) throws SQLException;
     }
+
 }
